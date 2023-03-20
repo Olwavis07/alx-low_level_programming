cd73faba9e1a785f25b0e8a0ab04f49f8bf226b6
Olwavis07 <olwavis2007@gmail.com>
2023-03-20 11:50:15 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -e entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+}
